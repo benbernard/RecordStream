c58a4f493e29002f58db403030b4c90c8bb41249
benbernard <github@benjaminbernard.com>
2011-09-04 13:23:23 -0700

    Change to sort the MANIFEST file

--- generate_manifest.sh
+++ generate_manifest.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-find . -type f -not -path ./Makefile -not -name MANIFEST -not -path './.git/*' | sed -e 's/^\.\///' >MANIFEST
+find . -type f -not -path ./Makefile -not -name MANIFEST -not -path './.git/*' | sed -e 's/^\.\///' | sort >MANIFEST
 
