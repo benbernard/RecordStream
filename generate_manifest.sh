a8c67b8cc2d2c608c707d62b4f0a4e66f22a3268
benbernard <github@benjaminbernard.com>
2011-09-04 17:42:07 -0700

    Updating MANIFEST

--- generate_manifest.sh
+++ generate_manifest.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-find . \( -type f -o -type l \) -not -path './makedeb/*' -not -path "./debian/*" -not -path "./blib/*" -not -path ./Makefile -not -name MANIFEST -not -path './.git/*' | sed -e 's/^\.\///' | sort >MANIFEST
+find . \( -type f -o -type l \) -not -name 'Makefile.old' -not -path './makedeb/*' -not -path "./debian/*" -not -path "./blib/*" -not -path ./Makefile -not -name MANIFEST -not -path './.git/*' | sed -e 's/^\.\///' | sort >MANIFEST
 
