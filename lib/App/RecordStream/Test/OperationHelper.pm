b5055ed2c0cb77e8a9f70b4e7ab8bd3f8d1aa290
Thomas Sibley <tsibley@cpan.org>
2014-10-19 22:53:19 -0700

    Extend OperationHelper to skip tests when optional operation deps are missing
    This is a cleaner spot to do optional dep checking in tests than copy
    and pasting specific operation deps and the code to check for them into
    each test script.

--- lib/App/RecordStream/Test/OperationHelper.pm
+++ lib/App/RecordStream/Test/OperationHelper.pm
@@ -8,6 +8,26 @@ use warnings;
 use Test::More;
 use App::RecordStream::InputStream;
 use App::RecordStream::OutputStream;
+use Carp qw(croak);
+
+sub import {
+  my $class = shift;
+
+  require App::RecordStream::OptionalRequire;
+  local $App::RecordStream::OptionalRequire::PRINT_WARNING = 0;
+
+  for my $op (@_) {
+    croak "invalid package name: '$op'"
+      unless $op =~ /^[a-z0-9_]+$/;
+    if (not eval "require App::RecordStream::Operation::$op; 1;") {
+      if ($@ =~ /Please install missing modules/) {
+        plan skip_all => "Missing deps for operation $op";
+      } else {
+        die $@;
+      }
+    }
+  }
+}
 
 sub new {
   my $class = shift;
